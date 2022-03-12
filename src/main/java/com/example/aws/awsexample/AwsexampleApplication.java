4943fb3c81022492280cb0b6d3a680918265c544
olal8 <41497395+olal8@users.noreply.github.com>
2022-03-12 07:49:01 -0600

    Add files via upload

---
+++ src/main/java/com/example/aws/awsexample/AwsexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws.awsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aws/awsexample/AwsexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aws.awsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
